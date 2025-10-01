27ff56831e942951492f5b1af064f9046f7a5004
Giulia Piombo <giulia.piombo01@gmail.com>
2025-10-01 14:37:02 -0500

    feat: skills translation

--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -1,21 +1,25 @@
 import enHero from '../resources/en/hero.json';
 import enExperience from '../resources/en/experience.json';
 import enProjects from '../resources/en/projects.json';
+import enSkills from '../resources/en/skills.json';
 
 import ptHero from '../resources/pt/hero.json';
 import ptExperience from '../resources/pt/experience.json';
 import ptProjects from '../resources/pt/projects.json';
+import ptSkills from '../resources/pt/skills.json';
 
 const translations = {
   en: {
     hero: enHero,
     experience: enExperience,
     projects: enProjects,
+    skills: enSkills,
   },
   pt: {
     hero: ptHero,
     experience: ptExperience,
     projects: ptProjects,
+    skills: ptSkills,
   },
 };
 
