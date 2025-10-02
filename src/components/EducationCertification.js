beb933e58aab7ee026cc66ae4ab817939945bb97
Giulia Piombo <giulia.piombo01@gmail.com>
2025-10-02 13:59:07 -0500

    feat: education certification translation

--- src/components/EducationCertification.js
+++ src/components/EducationCertification.js
@@ -6,73 +6,55 @@ import meta from '../images/meta.png';
 import aws from '../images/aws.png';
 import awsWhite from '../images/white-aws.png';
 import udemy from '../images/udemy.png';
+import { useTranslation } from '../hooks/useTranslation';
+import { useLanguage } from '../context/LanguageContext';
 
 const EducationCertification = ({ theme }) => {
+  const { lang } = useLanguage();
+  const t = useTranslation(lang, 'educationCertification');
+
   const awsImage = theme === 'dark' ? awsWhite : aws;
 
-  const certificates = [
-    {
-      logo: google,
-      title: 'Project Management',
-      link: 'https://www.coursera.org/account/accomplishments/specialization/MCCKS3QR5QL7',
-      date: 'Issued Oct 2024',
-    },
-    {
-      logo: meta,
-      title: 'Front-End Developer',
-      link: 'https://coursera.org/verify/professional-cert/YIRPC57GWMSE',
-      date: 'Issued Sep 2024',
-    },
-    {
-      logo: awsImage,
-      title: 'Certified Cloud Practitioner',
-      link: 'https://www.credly.com/badges/47a25b5c-8e9e-4f5b-ba9b-e838f005c6cd/linked_in_profile',
-      date: 'Issued Mar 2023',
-    },
-    {
-      logo: udemy,
-      title: 'React - The Complete Guide',
-      link: 'https://www.udemy.com/certificate/UC-dedd07af-796f-408d-a379-4f9983e3945f/',
-      date: 'Issued Mar 2023',
-    },
-    {
-      logo: udemy,
-      title: 'React Native',
-      link: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-a7187b09-d6c2-4a1b-8659-1feca2cd594b.pdf',
-      date: 'Issued Mar 2023',
-    },
-  ];
+  const imageMap = {
+    google,
+    meta,
+    awsImage,
+    udemy,
+  };
 
   return (
     <div className={classes.educationCertification}>
-      <h2>Education & Certification</h2>
+      <h2>{t.title}</h2>
       <div className={classes.column}>
         <div>
-          <h3>Things I achieved...</h3>
+          <h3>{t.educationSubtitle}</h3>
           <div className={classes.itemColumn}>
             <div className={classes.one}>
               <img src={ttu} width={200} />
             </div>
             <div className={classes.two}>
-              <h4>Texas Tech University</h4>
-              <p>August 2019 - December 2022</p>
-              <p>Bachelor's Degree in Computer Engineering</p>
-              <p>Honors College</p>
-              <p>Minors in Mathematics and Computer Science</p>
-              <p>
-                Founder and former president of BRASA at Tech Student
-                Organization
-              </p>
+              {t.education.map((item, idx) => (
+                <>
+                  <h4>{item.university}</h4>
+                  <p>{item.term}</p>
+                  <p>{item.degree}</p>
+                  {item.notes.map((note, idx) => (
+                    <>
+                      <p>{note}</p>
+                    </>
+                  ))}
+                </>
+              ))}
             </div>
           </div>
         </div>
         <div className={classes.certificates}>
-          <h3>Things I earned...</h3>
+          <h3>{t.certificationSubtitle}</h3>
           <div className={classes.certificatesList}>
-            {certificates.map((item, idx) => (
+            {t.certificates.map((item, idx) => (
               <CertificateItem
                 key={idx}
-                logo={item.logo}
+                logo={imageMap[item.logo]}
                 title={item.title}
                 link={item.link}
                 date={item.date}
--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -2,11 +2,13 @@ import enHero from '../resources/en/hero.json';
 import enExperience from '../resources/en/experience.json';
 import enProjects from '../resources/en/projects.json';
 import enSkills from '../resources/en/skills.json';
+import enEducationCertification from '../resources/en/education_certification.json';
 
 import ptHero from '../resources/pt/hero.json';
 import ptExperience from '../resources/pt/experience.json';
 import ptProjects from '../resources/pt/projects.json';
 import ptSkills from '../resources/pt/skills.json';
+import ptEducationCertification from '../resources/pt/education_certification.json';
 
 const translations = {
   en: {
@@ -14,12 +16,14 @@ const translations = {
     experience: enExperience,
     projects: enProjects,
     skills: enSkills,
+    educationCertification: enEducationCertification,
   },
   pt: {
     hero: ptHero,
     experience: ptExperience,
     projects: ptProjects,
     skills: ptSkills,
+    educationCertification: ptEducationCertification,
   },
 };
 
